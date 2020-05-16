82022e771e6a258b155d40521b2319965d8db3f0
chetan kumar <ckumarhcu@gmail.com>
2020-05-17 01:37:30 +0530

    find Longest ConseqSubseq

---
+++ longestConsecutive.py
@@ -0,0 +1,18 @@
+def findLongestConseqSubseq(arr , n):
+    s = set()
+    ans = 0
+
+    for ele in arr:
+        s.add(ele)
+    
+    for i in range(n):
+        if (arr[i]- 1) not in s:
+            j = arr[i]
+            while(j in s):
+                j+=1
+            ans = max(ans, j-arr[i])
+    return ans
+
+n = 7
+arr = [1, 9, 3, 10, 4, 20, 2] 
+findLongestConseqSubseq(arr, n)
